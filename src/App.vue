a2f900d25aafecf7f57be84fa510058ddce0e1ea
jabarbush <jabarbush@gmail.com>
2023-04-11 12:40:11 -0500

    Starting point

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div ref="svg"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as d3 from 'd3';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'D3.js and Vue!',
+    };
+  },
+  mounted() {
+    const svg = d3.select(this.$refs.svg)
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 500);
+
+    svg.append('circle')
+      .attr('cx', 250)
+      .attr('cy', 250)
+      .attr('r', 50)
+      .attr('fill', 'blue');
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h1 {
+  font-size: 2em;
+  font-weight: bold;
+  margin-bottom: 0.5em;
 }
 </style>
